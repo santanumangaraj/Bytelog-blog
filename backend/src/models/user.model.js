97231e7c5ade3b53b8616720886b0a580c0caffd
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-01-28 16:19:22 +0530

    feat: create user model with schema for username, fullName, email, avatar, password, and refreshToken

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose,{mongo, Schema} from "mongoose";
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim: true,
+        index: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase: true,
+        trim: true,
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+    },
+    refreshToken:{
+        type:String
+    }
+},{
+    timestamps:true,
+})
+
+export const User = mongoose.model("User",userSchema);

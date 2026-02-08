22b60b64b9fd7161f8ca7ca178ef7bd8c7c6d92b
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-02-08 21:15:38 +0530

    feat: add Font Awesome icons and update Login component styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../routes/api.js";
-import ByteLogLogo from "../component/ByteLog-Logo.jsx";
 
 const Login=()=>{
     const [err,setErr] = useState("")
@@ -40,8 +39,8 @@ const Login=()=>{
             <div className="text-center flex flex-col gap-2 my-10">
                 <img src="/ByteLog.svg" className="h-16 sm:h-20 md:h-24 mx-auto" alt="bytelog"/>
                 <h3 className="text-2xl sm:text-3xl font-semibold">Sign in</h3>
-                <p className="text-sm sm:text-base">or <a onClick={()=>navigate("/register")} className="text-cyan-500 cursor-pointer">Create an account</a></p>
-            </div>
+                <p className="text-sm sm:text-base">or <a onClick={()=>navigate("/register")} className="text-cyan-500 cursor-pointer hover:underline">Create an account</a></p>
+            </div>npm 
             <form className="w-full max-w-sm sm:max-w-md flex flex-col gap-3 py-4 px-4" onSubmit={handleSubmit}>
                 {err && 
                 <p className=" border-2 border-red-300 p-3 bg-red-100 text-sm rounded">
